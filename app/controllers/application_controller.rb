aa1a654d56ac55ca28d56a55a166686d8d47a6cd
Andres Torres <elfotografo007@gmail.com>
2014-03-13 17:56:28 -0500

    Ahora cada vez que un admin inicia sesión se redirige para usuarios

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,11 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :guardar_estadistica
- 
+  
+  def after_sign_in_path_for(administrador)
+    usuarios_path
+  end
+   
   private
   def guardar_estadistica
     est = Estadistica.find_or_create_by(_id: request.url)
