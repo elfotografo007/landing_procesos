5cc9b199deff7a41ae04edc2a5c0ea48c0758190
Andres Torres <elfotografo007@gmail.com>
2014-03-15 23:21:49 -0500

    Se escribe código para mantener la primera página que se le mostró al usuario.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,8 +2,18 @@ class WelcomeController < ApplicationController
   def index
     hits = Estadistica.find(request.url).hits
     @referer = params[:referer] || :ninguno
+    @usuario = Usuario.new
+    # Descomentar esto y borrar lo de abajo cuando deba mantener la primera pagina que se le mostro al usuario.
+    # if not cookies[:page].nil?
+    #   render cookies[:page]
+    # elsif hits.even?
+    #   cookies[:page] = :landing1
+    #   render :landing1
+    # else
+    #   cookies[:page] = :landing2
+    #   render :landing2
+    # end
     if hits.even?
-      @usuario = Usuario.new
       render :landing1
     else
       render :landing2
