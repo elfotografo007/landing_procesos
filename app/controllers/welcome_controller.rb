28c12d8f044de3a88b84bb3fc0366b1cde7715a1
Andres Torres <elfotografo007@gmail.com>
2014-03-25 07:25:46 -0500

    Se habilita A/B Testing

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,24 +4,24 @@ class WelcomeController < ApplicationController
     @referer = params[:referer] || :ninguno
     @usuario = Usuario.new
     # Descomentar esto y borrar lo de abajo cuando deba mantener la primera pagina que se le mostro al usuario.
-    # if not cookies[:page].nil?
-    #   @landing = cookies[:page]
-    #   render cookies[:page]
-    # elsif hits.even?
-    #   cookies[:page] = :landing1
-    #   @landing = :landing1
-    #   render :landing1
-    # else
-    #   cookies[:page] = :landing2
-    #   @landing = :landing2
-    #   render :landing2
-    # end
-    if hits.even?
+    if not cookies[:page].nil?
+      @landing = cookies[:page]
+      render cookies[:page]
+    elsif hits.even?
+      cookies[:page] = :landing1
       @landing = :landing1
       render :landing1
     else
+      cookies[:page] = :landing2
       @landing = :landing2
       render :landing2
     end
+  #if hits.even?
+  #  @landing = :landing1
+  #  render :landing1
+  #else
+  #  @landing = :landing2
+  #  render :landing2
+  #end
   end
 end
